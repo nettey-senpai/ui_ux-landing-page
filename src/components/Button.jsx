1e9e8e8a6146c17d8ec0ca29086c88a9864ba83a
Jeffrey <netteyjeff@gmail.com>
2024-02-06 16:53:34 +0000

    add business section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,12 @@
 const Button = () => {
   return (
-    <div>Button</div>
-  )
-}
+    <button
+      type="button"
+      className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none mt-10`}
+    >
+      Get Started
+    </button>
+  );
+};
 
-export default Button
+export default Button;
