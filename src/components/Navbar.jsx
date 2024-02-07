b2e2dfec34290605f519575f9a8b3c69bb74008f
Jeffrey <netteyjeff@gmail.com>
2024-02-07 14:39:39 +0000

    update sections ui

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = () => {
             key={nav.id}
             className={`font-poppins font-normal cursor-pointer text-[16px] ${
               index === navLinks.length - 1 ? "mr-0" : "mr-10"
-            } text-white hover:text-cyan-400`}
+            } text-white hover:text-secondary`}
           >
             <a href={`#${nav.id}`}>{nav.title}</a>
           </li>
@@ -40,7 +40,7 @@ const Navbar = () => {
                 key={nav.id}
                 className={`font-poppins font-normal cursor-pointer text-[16px] ${
                   index === navLinks.length - 1 ? "mr-0" : "mb-4"
-                } text-white hover:text-cyan-400`}
+                } text-white hover:text-secondary`}
               >
                 <a href={`#${nav.id}`}>{nav.title}</a>
               </li>
