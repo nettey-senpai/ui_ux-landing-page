00eeaef173de25a890e7558b2378f33176f43463
Jeffrey <netteyjeff@gmail.com>
2024-02-07 11:30:03 +0000

    add card deal section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,8 @@
-const Button = () => {
+const Button = ({ styles }) => {
   return (
     <button
       type="button"
-      className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none mt-10`}
+      className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none mt-10 ${styles} rounded-[10px]`}
     >
       Get Started
     </button>
