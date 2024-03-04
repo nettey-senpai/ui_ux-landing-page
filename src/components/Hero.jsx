b2e5e4debc174f38843c7a2152ef81f2d9d9027a
Jeffrey <netteyjeff@gmail.com>
2024-03-04 16:45:20 +0000

    update get started button ui

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -47,7 +47,7 @@ const Hero = () => (
       <div className="absolute z-[1] w-[80%] h-[80%] rounded-full white__gradient bottom-40" />
       <div className="absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 blue__gradient" />
     </div>
-    <div className={`ss:hidden ${styles.flexCenter}`}>
+    <div className={`ss:hidden ${styles.flexCenter} hover:animate-bounce`}>
       <GetStarted />
     </div>
   </section>
